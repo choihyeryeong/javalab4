129ff5f43ddf5c6749992fdca54ca0dfeecdad22
choihyeryeong <hr_95@naver.com>
2015-10-01 18:27:28 +0900

    hw2

--- src/kr/ac/kookmin/embedded/api/ArrayListMagnet.java
+++ src/kr/ac/kookmin/embedded/api/ArrayListMagnet.java
@@ -22,16 +22,21 @@ public class ArrayListMagnet {
 		a.add(2,"two");
 		a.add(3,"three");
 		printAL(a);
+		
 		a.remove(2);
-		if(a.contains("two")){
-			a.add("2.2");
-		}
+
 		if(a.contains("three")){
 			a.add("four");
 		}
 		printAL(a);
+		
 		if(a.indexOf("four") != 4) {
 			a.add(4, "4.2");
+		}
+		printAL(a);
+		
+		if(a.contains("two")){
+			a.add("2.2");
 		}
 		printAL(a);
 	}
