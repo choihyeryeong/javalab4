0c160651fb2ae417608f9474008c42731c667cd3
choihyeryeong <hr_95@naver.com>
2015-10-01 16:57:30 +0900

    hw3

--- src/kr/ac/kookmin/embedded/overriding/MonsterTestDrive.java
+++ src/kr/ac/kookmin/embedded/overriding/MonsterTestDrive.java
@@ -2,7 +2,8 @@ package kr.ac.kookmin.embedded.overriding;
 
 public class MonsterTestDrive {
 	public static void main(String [] args){
-		Monster [] ma = new Monster[3];
+		Monster [] ma = new Monster[3]; 
+		
 		ma[0] = new Vampire();
 		ma[1] = new Dragon();
 		ma[2] = new Monster();
